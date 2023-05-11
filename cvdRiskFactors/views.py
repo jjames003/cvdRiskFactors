62456e505b9ef384f965cefb8c1e252f13491104
jjames003 <jjames003@citymail.cuny.edu>
2023-05-10 20:04:55 -0400

    Updated version of project

--- cvdRiskFactors/views.py
+++ cvdRiskFactors/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from modelTraining.main import CardioRiskPredictor
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        age = int(request.POST.get('age'))
+        gender = int(request.POST.get('gender'))
+        height = float(request.POST.get('height'))
+        weight = float(request.POST.get('weight'))
+        ap_hi = int(request.POST.get('ap_hi'))
+        ap_lo = int(request.POST.get('ap_lo'))
+        cholesterol = int(request.POST.get('cholesterol'))
+        gluc = int(request.POST.get('gluc'))
+        smoke = int(request.POST.get('smoke'))
+        alco = int(request.POST.get('alco'))
+        active = int(request.POST.get('active'))
+        
+        predictor = CardioRiskPredictor(model_file="modelTraining/model.pkl")
+        percentage = predictor.predict(age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active)
+        
+        return render(request, 'results.html', {'percentage': percentage})
+    else:
+        return render(request, 'home.html')
 
+def results(request):
+    return render(request, 'results.html')
--- modelTraining/main.py
+++ modelTraining/main.py
@@ -1,80 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pickle
 
+class CardioRiskPredictor:
+    def __init__(self, model_file='model.pkl'):
+        with open(model_file, 'rb') as f:
+            self.pickled_model = pickle.load(f)
 
-# In[2]:
-
-
-pickled_model = pickle.load(open('model.pkl', 'rb'))
-
-
-# In[3]:
-
-
-"""
-Age should be int between 1-100
-"""
-age = 85
-
-"""
-Gender should be int 1 (Male) or 2 (Female)
-"""
-gender = 1
-
-"""
-Height should be between 50-280 cm
-"""
-height = 100
-
-"""
-Weight should be between 15-125 kg
-"""
-weight = 60
-
-"""
-Systolic Blood Pressure should be between 100-200 mm Hg
-"""
-ap_hi = 150
-
-"""
-Diastolic Blood Pressure should be between 70-130 mm Hg
-"""
-ap_low = 90
-
-"""
-Cholesterol has three levels, 1-3. Should be a drop down menu
-"""
-chol = 1
-
-"""
-Glucose levels has three levels, 1-3. Should be a drop down menu
-"""
-gluc = 1
-
-"""
-Smoke, Alcohol, and Active should be are 1 (yes) or 0 (no)
-"""
-smoke = 0
-alco = 0
-active = 0
-
-
-# In[17]:
-
-
-array = [[age,gender,height,weight,ap_hi,ap_low,chol,gluc,smoke,alco,active]]
-pickled_model.predict(array)
-per = pickled_model.predict_proba(array)[:,1][0]
-per = per.round(4)*100
-
-
-# In[18]:
-
-
-print("You have a ",per,"% chance of having cardiovascular disease")
-
+    def predict(self, age, gender, height, weight, ap_hi, ap_low, chol, gluc, smoke, alco, active):
+        """
+        Age should be int between 1-100
+        Gender should be int 1 (Male) or 2 (Female)
+        Height should be between 50-280 cm
+        Weight should be between 15-125 kg
+        Systolic Blood Pressure should be between 100-200 mm Hg
+        Diastolic Blood Pressure should be between 70-130 mm Hg
+        Cholesterol has three levels, 1-3. Should be a drop down menu
+        Glucose levels has three levels, 1-3. Should be a drop down menu
+        Smoke, Alcohol, and Active should be are 1 (yes) or 0 (no)
+        """
+        array = [[age, gender, height, weight, ap_hi, ap_low, chol, gluc, smoke, alco, active]]
+        per = self.pickled_model.predict_proba(array)[:, 1][0]
+        return round(per * 100, 2)
